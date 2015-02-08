6c7d79827875ed8d0c46927be9c36a80df5096c0
Z0NEN <z0nen@hotmail.com>
2015-02-08 11:29:06 +0700

    Sunshine

---
+++ app/src/androidTest/java/tk/nas_project/sunshine/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tk.nas_project.sunshine;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
